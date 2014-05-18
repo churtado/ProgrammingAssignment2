4b2a7d6e8d6a1c3b743c88d57c8b7f5e18c7f273
churtado <charliepapa2@gmail.com>
2014-05-18 17:06:14 +0200

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2 - Lexical Scoping
+## The following program creates a special type of "matrix" that can cache its own inverse
+## It is useful for saving computational effort of obtaining the inverse of a matrix for which the inverse was previously calculated
+## In reality, the "matrix" is a list of functions that allow for caching the inverse
+
+## In summary, its an exercise on how to use closures in R
+
 
 ## Write a short comment describing this function
-
+## In this function, we define a method to get the matrix itself, get its inverse, or set the value of the matrix or its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
+      #Set the inverse initially to null
+      i<-NULL
+      
+      #Set the matrix to be equal to what is passed in y
+      set <- function(y){
+            x <<- y
+            i <<- NULL
+      }
+      
+      #Simply return the matrix 
+      get <- function() x
+      
+      #Set the inverse to the value passed as a parameter to the function
+      setinverse <- function(inverse) i <<- inverse
+      
+      #Simple return the inverse
+      getinverse <- function() i
+      
+      #Return a list of all of these functions
+      list(set = set, get = get, setinverse=setinverse, getinverse=getinverse)
+  
 }
 
 
-## Write a short comment describing this function
-
+## This function will try to see if the inverse of a matrix of the type we previously defined has an inverse
+## If it finds the inverse has been calculated, it will return the inverse
+## If no inverse is found, the inverse is calculated with the solve() function, set in our special "matrix"
+## Lastly, the function returns the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+      ## Call the getinverse function on our special "matrix" to see if the inverse has already been calculated
+      i <- x$getinverse()
+    
+      ## If the value of the inverse is calculated (it's not NULL), simply return it and we're done
+      if(!is.null(i)){
+            return(i)
+      }
+      
+      ## If the return statment was not executed, the rest of the function gets executed
+      
+      ## Obtain the matrix value with its get() function
+      matrix <- x$get()
+      
+      ## Use the solve() function to obtain the inverse
+      i <- solve(x)
+      
+      ## Set the inverse into our special "matrix"
+      x$setinverse(i)
+      
+      ## Return the inverse
+      i
 }
+
+
+
+
