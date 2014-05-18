7ec0aa135606744db461c659ada59e44dce379cf
churtado <charliepapa2@gmail.com>
2014-05-18 17:49:37 +0200

    bug fix

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
 ## Programming Assignment 2 - Lexical Scoping
 ## The following program creates a special type of "matrix" that can cache its own inverse
-## It is useful for saving computational effort of obtaining the inverse of a matrix for which the inverse was previously calculated
-## In reality, the "matrix" is a list of functions that allow for caching the inverse
+## It is useful for saving computational effort of obtaining the inverse of a matrix for 
+## which the inverse was previously calculated.
+## In reality, the "matrix" is a list of functions that allow for caching the inverse.
 
 ## In summary, its an exercise on how to use closures in R
 
-
-## Write a short comment describing this function
-## In this function, we define a method to get the matrix itself, get its inverse, or set the value of the matrix or its inverse.
+## In this function, we define a method to get the matrix itself, get its inverse, or set 
+## the value of the matrix or its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
       #Set the inverse initially to null
@@ -34,10 +34,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function will try to see if the inverse of a matrix of the type we previously defined has an inverse
-## If it finds the inverse has been calculated, it will return the inverse
-## If no inverse is found, the inverse is calculated with the solve() function, set in our special "matrix"
-## Lastly, the function returns the inverse
+## This function will try to see if the inverse of a 
+## matrix of the type we previously defined has an inverse.
+## If it finds the inverse has been calculated, it will return the inverse.
+## If no inverse is found, the inverse is calculated with the solve() function, 
+## set in our special "matrix". Lastly, the function returns the inverse.
 cacheSolve <- function(x, ...) {
   
       ## Call the getinverse function on our special "matrix" to see if the inverse has already been calculated
@@ -63,7 +64,3 @@ cacheSolve <- function(x, ...) {
       ## Return the inverse
       i
 }
-
-
-
-
